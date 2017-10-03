14ef09c9e01f9dd3d14f5db85ec73febd0c2928d
Andrew Smith <andy@robotlovesyou.com>
2017-10-03 21:48:40 +0200

    Tidy shell files

--- runexplore.sh
+++ runexplore.sh
@@ -1,3 +1,5 @@
 #! /usr/bin/env bash
 set -euxo pipefail
-docker run --rm -it --volume="$(pwd)":/project --volume="$(pwd)/dataset":/dataset --workdir=/project mlcap:latest python explore.py
+docker run --rm -it --volume="$(pwd)":/project \
+--volume="$(pwd)/dataset":/dataset \
+--workdir=/project mlcap:latest python explore.py
--- runorig.sh
+++ runorig.sh
@@ -1,3 +1,6 @@
 #! /usr/bin/env bash
 set -euxo pipefail
-docker run --rm -it --volume="$(pwd)":/project --volume="$(pwd)/dataset":/dataset --volume="$(pwd)/output":/output --workdir=/project mlcap:latest python main.py original 1
+docker run --rm -it --volume="$(pwd)":/project \
+--volume="$(pwd)/dataset":/dataset \
+--volume="$(pwd)/output":/output \
+--workdir=/project mlcap:latest python main.py original 1
